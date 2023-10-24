91bec38829349aeaac846b03cc2a0556a529eb8f
shunyue <1277952981@qq.com>
2023-10-24 16:10:39 +0800

    chore: 初始化 button 组件 demo 页面

---
+++ packages/vant/src/button/demo/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import VanButton from '..';
+</script>
+
+<template>
+  <demo-block title="按钮类型">
+    <div class="demo-button-row">
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="success">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+    </div>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+  </demo-block>
+</template>
